ccaaa257e8d3b682c5045e14b3e0ae45141f4e2c
Feiwar <143819364+Feiwar@users.noreply.github.com>
2023-10-10 12:07:51 +0300

    Add files via upload

---
+++ VVV.pas
@@ -0,0 +1,17 @@
+﻿Program VVV;
+uses
+  Math;
+var
+  x: real;
+  result: real;
+begin
+  writeln('Введите значение x:');
+  readln(x);
+  if x < -6 then
+    result := (6 / log2(x)) * exp(x / 100)
+  else if (x >= -6) and (x < -2) then
+    result := tan(x) * log10(x)
+  else
+    result := power(x, 2) * ln(x) - ln(x);
+  writeln('Значение функции: ', result);
+end.
---
+++ ZZZ.pas
@@ -0,0 +1,19 @@
+﻿program ZZZ;
+uses
+  Math;
+var
+  x, result: real;
+begin
+  x := -8.0;
+  while x <= 0.0 do
+  begin
+    if x < -6 then
+      result := (6 / log2(-x)) * exp(-x / 100)
+    else if (x >= -6) and (x < -2) then
+      result := tan(-x) * log10(-x)
+ else
+      result := power(-x, 2) * ln(-x) - ln(-x);
+    writeln('x = ', x:0:1, ', Результат вычисления функции: ', result:0:3);
+    x := x + 0.2;
+  end;
+end.
